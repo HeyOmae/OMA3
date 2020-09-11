7a5757d21c0f65ebe33cbcb7834c4afa9aea579c
dethstrobe <dethstrobe@gmail.com>
2020-09-10 23:08:46 -0400

    generating runner DB

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,29 @@
 import { AppProps } from "next/app"
 import CssBaseline from "@material-ui/core/CssBaseline"
 
+if (typeof window !== "undefined") {
+  import("react-indexed-db").then(({ initDB }) => {
+    initDB({
+      name: "omae",
+      version: 1,
+      objectStoresMeta: [
+        {
+          store: "runners",
+          storeConfig: { keyPath: "id", autoIncrement: true },
+          storeSchema: [
+            { name: "name", keypath: "name", options: { unique: true } },
+            {
+              name: "description",
+              keypath: "description",
+              options: { unique: false },
+            },
+          ],
+        },
+      ],
+    })
+  })
+}
+
 const App = ({ Component, pageProps }: AppProps) => (
   <>
     <CssBaseline />
