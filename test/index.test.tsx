34410c9676b35ac4950be91a498f3a6b34a9d1c8
dethstrobe <dethstrobe@gmail.com>
2020-09-13 06:53:50 -0400

    added higher order component to get runner from db

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -2,16 +2,23 @@ import React from "react"
 import { render, fireEvent } from "./testUtils"
 import { Home } from "../pages/index"
 
-describe("Home page", () => {
-  xit("matches snapshot", () => {
-    const { asFragment } = render(<Home />, {})
-    expect(asFragment()).toMatchSnapshot()
-  })
+const mockAddToIDB = jest.fn()
+jest.mock("react-indexed-db", () => ({
+  useIndexedDB: () => ({ add: mockAddToIDB }),
+}))
 
+describe("Home page", () => {
   it("clicking button triggers alert", () => {
     const { getByText } = render(<Home />, {})
     window.alert = jest.fn()
     fireEvent.click(getByText("Test Button"))
     expect(window.alert).toHaveBeenCalledWith("With typescript and Jest")
+  })
+
+  xit("should add a new runner to the indexed db", () => {
+    const { getByText } = render(<Home />)
+    fireEvent.click(getByText("Create Runner"))
+
+    expect(mockAddToIDB).toHaveBeenCalled()
   })
 })
