d1303b7c043d684ef73a535f2b9aecea7ac7ff12
dethstrobe <dethstrobe@gmail.com>
2020-09-15 22:50:54 -0400

    refactor setupIndexedDb fuction to make it more reuseable

---
+++ test/mocks.ts
@@ -0,0 +1,20 @@
+import { Runner } from "../types/runner"
+
+export const mockedRunners: Runner[] = [
+  {
+    id: 1,
+    name: "Bull",
+    description: "The best ork decker you never met.",
+  },
+  {
+    id: 2,
+    name: "Man-of-many-names",
+    description: "Not knowns by any other name",
+  },
+  {
+    id: 3,
+    name: "FastJack",
+    description:
+      "He was born in the 1990's and now has an AI living in his brain.",
+  },
+]
