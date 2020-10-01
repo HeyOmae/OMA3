3713c0ba79928135d647019d85675e353a0aa776
dethstrobe <dethstrobe@gmail.com>
2020-10-01 11:15:20 -0400

    add unit test for priority page

--- test/[id]/priority.test.tsx
+++ test/[id]/priority.test.tsx
@@ -1,14 +1,23 @@
 import { PriorityPage } from "../../pages/[id]/priority"
-import { render, withTestRouter } from "../testUtils"
+import { render, withTestRouter, waitFor, setupIndexedDB } from "../testUtils"
 
 describe("Priority Page", () => {
+  beforeAll(setupIndexedDB)
   const setup = () => {
     return render(withTestRouter(<PriorityPage />, { query: { id: "1" } }))
   }
 
-  xit("should exist", () => {
+  it("should exist", async () => {
     const { getByText } = setup()
 
     expect(getByText("Priority Table")).toBeInTheDocument()
+
+    await waitFor(() => {
+      expect(getByText("Metatype")).toBeInTheDocument()
+      expect(getByText("Attributes")).toBeInTheDocument()
+      expect(getByText("Skills")).toBeInTheDocument()
+      expect(getByText("Mag/Res")).toBeInTheDocument()
+      expect(getByText("Resources")).toBeInTheDocument()
+    })
   })
 })
