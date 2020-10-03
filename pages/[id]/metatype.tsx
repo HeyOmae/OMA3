daadd7e3bcb2a570099dbfb3200744b028d00d25
dethstrobe <dethstrobe@gmail.com>
2020-10-03 15:19:04 -0400

    add metatype placeholder page

---
+++ pages/[id]/metatype.tsx
@@ -0,0 +1,9 @@
+import { RunnerLayout } from "../../components/layout"
+
+export const MetatypePage = () => (
+  <RunnerLayout>
+    <h1>Metatype</h1>
+  </RunnerLayout>
+)
+
+export default MetatypePage
