ae5fff60ce644f8581f148825272f7da9527f285
dethstrobe <dethstrobe@gmail.com>
2020-10-06 09:32:34 -0400

    add sliders to select attributes

--- components/runner/metatype/metatype.test.tsx
+++ components/runner/metatype/metatype.test.tsx
@@ -12,7 +12,7 @@ describe("<Metatype/>", () => {
   const setup = () =>
     render(withTestRouter(<Metatype />, { query: { id: "1" } }))
   it("should have a radio buttons with the metatypes", async () => {
-    const { getByRole } = setup()
+    const { getByRole, queryByText } = setup()
 
     await waitFor(() => {
       const metatypeRadio = getByRole("radiogroup", { name: "metatypes" })
@@ -22,6 +22,17 @@ describe("<Metatype/>", () => {
       expect(getByLabelText(metatypeRadio, "Elf")).toBeInTheDocument()
       expect(getByLabelText(metatypeRadio, "Ork")).toBeInTheDocument()
       expect(getByLabelText(metatypeRadio, "Troll")).toBeInTheDocument()
+
+      // if metatype is not selected attributes should not display
+      expect(queryByText("Body")).not.toBeInTheDocument()
+      expect(queryByText("Agility")).not.toBeInTheDocument()
+      expect(queryByText("Reaction")).not.toBeInTheDocument()
+      expect(queryByText("Strength")).not.toBeInTheDocument()
+      expect(queryByText("Willpower")).not.toBeInTheDocument()
+      expect(queryByText("Logic")).not.toBeInTheDocument()
+      expect(queryByText("Intuition")).not.toBeInTheDocument()
+      expect(queryByText("Charisma")).not.toBeInTheDocument()
+      expect(queryByText("Edge")).not.toBeInTheDocument()
     })
   })
 
@@ -48,6 +59,31 @@ describe("<Metatype/>", () => {
       expect(
         (getByLabelText(metatypeRadio, "Ork") as HTMLInputElement).checked
       ).toBe(true)
+    })
+  })
+
+  describe("attributes sliders", () => {
+    it("should be visible if metatype is selected", async () => {
+      const { getByLabelText, getByText } = setup()
+      await waitFor(() => {
+        getByLabelText("Ork").click()
+      })
+      expect(
+        indexedDB._databases.get("omae").rawObjectStores.get("runners").records
+          .records[0].value.metatype
+      ).toEqual("Ork")
+
+      await waitFor(() => {
+        expect(getByText("Body")).toBeInTheDocument()
+        expect(getByText("Agility")).toBeInTheDocument()
+        expect(getByText("Reaction")).toBeInTheDocument()
+        expect(getByText("Strength")).toBeInTheDocument()
+        expect(getByText("Willpower")).toBeInTheDocument()
+        expect(getByText("Logic")).toBeInTheDocument()
+        expect(getByText("Intuition")).toBeInTheDocument()
+        expect(getByText("Charisma")).toBeInTheDocument()
+        expect(getByText("Edge")).toBeInTheDocument()
+      })
     })
   })
 })
