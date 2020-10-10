48ee60850c31620e5f04ee4c7752bfd937faaca6
dethstrobe <dethstrobe@gmail.com>
2020-10-10 16:34:54 -0400

    displayPoints component calculates how many points are left ot spend

--- components/runner/metatype/DisplayPoints/index.tsx
+++ components/runner/metatype/DisplayPoints/index.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react"
+import { FC, useMemo } from "react"
 import { Runner } from "../../../../types/runner"
 import priorityData from "../../../../data/priorityTable.json"
 
@@ -6,13 +6,30 @@ export interface Props {
   runner: Runner
 }
 
-export const DisplayPoints: FC<Props> = ({ runner }) => (
-  <dl>
-    <dt>Adjustment Points</dt>
-    <dd>
-      {priorityData.metatypes[runner.priority.metatype]?.adjustmentPoints}
-    </dd>
-    <dt>Attribute Points</dt>
-    <dd>{priorityData.attributes[runner.priority.attributes]}</dd>
-  </dl>
-)
+export const DisplayPoints: FC<Props> = ({ runner }) => {
+  const [adjustmentSpend, attributeSpend] = useMemo<[number, number]>(
+    () =>
+      Object.values(runner.attributes ?? {}).reduce(
+        (accumulator, { adjustment, points }) => [
+          accumulator[0] + adjustment,
+          accumulator[1] + points,
+        ],
+        [0, 0]
+      ),
+    [runner.attributes]
+  )
+  return (
+    <dl>
+      <dt>Adjustment Points</dt>
+      <dd>
+        {(priorityData.metatypes[runner.priority.metatype]?.adjustmentPoints ??
+          0) - adjustmentSpend}
+      </dd>
+      <dt>Attribute Points</dt>
+      <dd>
+        {(priorityData.attributes[runner.priority.attributes] ?? 0) -
+          attributeSpend}
+      </dd>
+    </dl>
+  )
+}
