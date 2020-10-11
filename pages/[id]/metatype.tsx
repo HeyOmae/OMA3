94322832d261c5ac5c2c53621f6a1b68fc3691a8
dethstrobe <dethstrobe@gmail.com>
2020-10-11 11:00:22 -0400

    update metatype component in metatype page

--- pages/[id]/metatype.tsx
+++ pages/[id]/metatype.tsx
@@ -1,8 +1,14 @@
 import { RunnerLayout } from "../../components/layout"
+import dynamic from "next/dynamic"
+
+const Metatype = dynamic(() => import("../../components/runner/metatype"), {
+  ssr: false,
+})
 
 export const MetatypePage = () => (
   <RunnerLayout>
     <h1>Metatype</h1>
+    <Metatype />
   </RunnerLayout>
 )
 
--- test/[id]/metatype.test.tsx
+++ test/[id]/metatype.test.tsx
@@ -1,11 +1,22 @@
-import { render, withTestRouter } from "../testUtils"
+import { render, withTestRouter, waitFor, setupIndexedDB } from "../testUtils"
 import { MetatypePage } from "../../pages/[id]/metatype"
 
 describe("Metatype Page", () => {
-  const setup = () => render(withTestRouter(<MetatypePage />))
-  it("should exist", () => {
+  beforeAll(setupIndexedDB)
+  const setup = () =>
+    render(withTestRouter(<MetatypePage />, { query: { id: "3" } }))
+  it("should exist", async () => {
     const { getByText } = setup()
 
     expect(getByText("Metatype")).toBeInTheDocument()
+
+    await waitFor(() => {
+      expect(getByText("Metatypes")).toBeInTheDocument()
+      expect(getByText("Human")).toBeInTheDocument()
+      expect(getByText("Dwarf")).toBeInTheDocument()
+      expect(getByText("Elf")).toBeInTheDocument()
+      expect(getByText("Ork")).toBeInTheDocument()
+      expect(getByText("Troll")).toBeInTheDocument()
+    })
   })
 })
