9e9e495702f043bf38fac842ba9350ca0ce34416
dethstrobe <dethstrobe@gmail.com>
2020-11-21 22:41:58 -0500

    overly complicated mystic adept power point display logic

--- components/runner/magRes/RemainingSpells/index.tsx
+++ components/runner/magRes/RemainingSpells/index.tsx
@@ -23,18 +23,26 @@ export const RemainingSpells: FC<Props> = ({
       () =>
         Object.values(spells).reduce(
           (total, spellsInCategory) => total + spellsInCategory.length,
-          0,
+          rituals.length,
         ),
-      [spells],
+      [rituals],
     ),
-    remainingNumberOfSpells = totalSpells - numberOfKnownSpells - rituals.length
+    remainingNumberOfSpells = totalSpells - numberOfKnownSpells
 
-  const totalMagic = rating + adjustmentPoints,
-    remainingPowerPoints = powers.reduce(
-      (accumulator, { levels, level, cost }) =>
-        accumulator - (levels ? (level ?? 1) * cost : cost),
-      totalMagic,
-    )
+  const [powerPointsTotal, remainingPowerPoints] = useMemo(() => {
+    const powerPointsTotal =
+      rating +
+      adjustmentPoints -
+      Math.round((magRes === "Mystic Adept" ? numberOfKnownSpells : 0) / 2)
+    return [
+      powerPointsTotal,
+      powers.reduce(
+        (accumulator, { levels, level, cost }) =>
+          accumulator - (levels ? (level ?? 1) * cost : cost),
+        powerPointsTotal,
+      ),
+    ]
+  }, [magRes, rating, adjustmentPoints, numberOfKnownSpells, powers])
 
   return (
     <dl>
@@ -49,8 +57,8 @@ export const RemainingSpells: FC<Props> = ({
       {(magRes === "Adept" || magRes === "Mystic Adept") && (
         <>
           <dt>Power Points Left</dt>
-          <dd>
-            {remainingPowerPoints}/{totalMagic}
+          <dd className={remainingPowerPoints < 0 ? "bad-stuff" : ""}>
+            {remainingPowerPoints}/{powerPointsTotal}
           </dd>
         </>
       )}
