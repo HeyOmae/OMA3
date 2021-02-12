8ab630c74f6aaacbe97dcfc27594ed2a13fa536c
dethstrobe <dethstrobe@gmail.com>
2021-02-11 23:11:10 -0500

    add tools data added placeholder data for sensors

---
+++ data/tools.ts
@@ -0,0 +1,38 @@
+export const tools = [
+  {
+    useAs: [
+      {
+        type: "TOOLS",
+        subtype: "TOOLS",
+      },
+    ],
+    availability: "1",
+    cost: 500,
+    name: "Kit",
+    choice: "SKILL",
+  },
+  {
+    useAs: [
+      {
+        type: "TOOLS",
+        subtype: "TOOLS",
+      },
+    ],
+    availability: "4",
+    cost: 5000,
+    name: "Shop",
+    choice: "SKILL",
+  },
+  {
+    useAs: [
+      {
+        type: "TOOLS",
+        subtype: "TOOLS",
+      },
+    ],
+    availability: "7",
+    cost: 50000,
+    name: "Facility",
+    choice: "SKILL",
+  },
+]
