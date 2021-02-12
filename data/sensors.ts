0d52dcd84757bfa6ceb17a911aeef51b08564943
dethstrobe <dethstrobe@gmail.com>
2021-02-11 23:11:10 -0500

    add tools data added placeholder data for sensors

---
+++ data/sensors.ts
@@ -0,0 +1,860 @@
+export const sensors = [
+  {
+    modifications: {
+      itemhookmod: {
+        _capacity: "1",
+        _hook: "OPTICAL",
+      },
+    },
+    _avail: "1",
+    _cost: "50",
+    _id: "binoculars",
+    _maxrat: "3",
+    _rate: "true",
+    _ratemul: "CAPACITY PRICE",
+    _subtype: "IMAGING",
+    _type: "ELECTRONICS",
+  },
+  {
+    _avail: "1",
+    _cost: "50",
+    _id: "binoculars_optical",
+    _subtype: "OPTICAL",
+    _type: "ELECTRONICS",
+  },
+  {
+    useas: [
+      {
+        _type: "ELECTRONICS",
+        _subtype: "IMAGING",
+      },
+    ],
+    modifications: {
+      itemhookmod: {
+        _capacity: "1",
+        _hook: "OPTICAL",
+      },
+    },
+    _avail: "1",
+    _cost: "100",
+    _id: "camera",
+    _maxrat: "6",
+    _rate: "true",
+    _ratemul: "CAPACITY PRICE",
+  },
+  {
+    modifications: {
+      itemhookmod: {
+        _capacity: "1",
+        _hook: "OPTICAL",
+      },
+    },
+    _avail: "1",
+    _cost: "100",
+    _id: "microcamera",
+    _subtype: "IMAGING",
+    _type: "ELECTRONICS",
+  },
+  {
+    modifications: {
+      itemhookmod: {
+        _capacity: "1",
+        _hook: "OPTICAL",
+      },
+    },
+    _avail: "2",
+    _cost: "200",
+    _id: "contacts",
+    _maxrat: "3",
+    _rate: "true",
+    _ratemul: "CAPACITY PRICE",
+    _subtype: "IMAGING",
+    _type: "ELECTRONICS",
+  },
+  {
+    _avail: "3",
+    _cost: "250",
+    _id: "endoscope",
+    _subtype: "OPTICAL",
+    _type: "ELECTRONICS",
+  },
+  {
+    modifications: {
+      itemhookmod: {
+        _capacity: "1",
+        _hook: "OPTICAL",
+      },
+    },
+    _avail: "1",
+    _cost: "100",
+    _id: "glasses",
+    _maxrat: "4",
+    _rate: "true",
+    _ratemul: "CAPACITY PRICE",
+    _subtype: "IMAGING",
+    _type: "ELECTRONICS",
+  },
+  {
+    useas: [
+      {
+        _type: "ELECTRONICS",
+        _subtype: "IMAGING",
+      },
+    ],
+    modifications: {
+      itemhookmod: {
+        _capacity: "1",
+        _hook: "OPTICAL",
+      },
+    },
+    _avail: "1",
+    _cost: "50",
+    _id: "goggles",
+    _maxrat: "6",
+    _rate: "true",
+    _ratemul: "CAPACITY PRICE",
+  },
+  {
+    modifications: {
+      itemhookmod: {
+        _capacity: "3",
+        _hook: "OPTICAL",
+      },
+    },
+    _avail: "1",
+    _cost: "350",
+    _id: "imaging_scope_single",
+    _subtype: "IMAGING",
+    _type: "ELECTRONICS",
+  },
+  {
+    _avail: "5",
+    _cost: "3000",
+    _id: "mage-sight-goggles",
+    _subtype: "OPTICAL",
+    _type: "ELECTRONICS",
+  },
+  {
+    modifications: {
+      itemhookmod: {
+        _capacity: "1",
+        _hook: "OPTICAL",
+      },
+    },
+    _avail: "1",
+    _cost: "120",
+    _id: "monocle",
+    _maxrat: "4",
+    _rate: "true",
+    _ratemul: "CAPACITY PRICE",
+    _subtype: "IMAGING",
+    _type: "ELECTRONICS",
+  },
+  {
+    useas: [
+      {
+        _type: "ACCESSORY",
+        _subtype: "OPTICAL",
+        _cap: "1",
+        _slot: "OPTICAL",
+      },
+      {
+        _type: "ACCESSORY",
+        _subtype: "CYBER_EYEWARE",
+        _cap: "1",
+        _slot: "CYBEREYE_IMPLANT",
+        _cost: "750",
+        _avail: "2",
+      },
+      {
+        _type: "CYBERWARE",
+        _subtype: "CYBER_EYEWARE",
+        _ess: "0.1",
+        _cost: "750",
+        _avail: "2",
+      },
+      {
+        _type: "ACCESSORY",
+        _subtype: "OPTICAL",
+        _slot: "HELMET_ACCESSORY",
+        _cap: "1",
+      },
+    ],
+    _avail: "1",
+    _cost: "250",
+    _id: "flare_compensation",
+  },
+  {
+    useas: [
+      {
+        _type: "ACCESSORY",
+        _subtype: "OPTICAL",
+        _cap: "1",
+        _slot: "OPTICAL",
+      },
+      {
+        _type: "ACCESSORY",
+        _subtype: "CYBER_EYEWARE",
+        _cap: "1",
+        _slot: "CYBEREYE_IMPLANT",
+        _cost: "775",
+        _avail: "2",
+      },
+      {
+        _type: "CYBERWARE",
+        _subtype: "CYBER_EYEWARE",
+        _ess: "0.1",
+        _cost: "775",
+        _avail: "2",
+      },
+      {
+        _type: "ACCESSORY",
+        _subtype: "OPTICAL",
+        _slot: "HELMET_ACCESSORY",
+        _cap: "1",
+      },
+    ],
+    _avail: "1",
+    _cost: "25",
+    _id: "image_link",
+  },
+  {
+    useas: [
+      {
+        _type: "ACCESSORY",
+        _subtype: "OPTICAL",
+        _cap: "1",
+        _slot: "OPTICAL",
+      },
+      {
+        _type: "ACCESSORY",
+        _subtype: "CYBER_EYEWARE",
+        _cap: "2",
+        _slot: "CYBEREYE_IMPLANT",
+        _cost: "1000",
+        _avail: "2",
+      },
+      {
+        _type: "CYBERWARE",
+        _subtype: "CYBER_EYEWARE",
+        _ess: "0.1",
+        _cost: "1000",
+        _avail: "2",
+      },
+      {
+        _type: "ACCESSORY",
+        _subtype: "OPTICAL",
+        _slot: "HELMET_ACCESSORY",
+        _cap: "1",
+      },
+    ],
+    _avail: "2",
+    _cost: "500",
+    _id: "low_light_vision",
+  },
+  {
+    useas: [
+      {
+        _type: "ACCESSORY",
+        _subtype: "OPTICAL",
+        _cap: "2",
+        _slot: "OPTICAL",
+      },
+      {
+        _type: "ACCESSORY",
+        _subtype: "CYBER_EYEWARE",
+        _cap: "3",
+        _slot: "CYBEREYE_IMPLANT",
+        _cost: "2000",
+        _avail: "3L",
+      },
+      {
+        _type: "CYBERWARE",
+        _subtype: "CYBER_EYEWARE",
+        _ess: "0.2",
+        _cost: "2000",
+        _avail: "3L",
+      },
+      {
+        _type: "ACCESSORY",
+        _subtype: "OPTICAL",
+        _slot: "HELMET_ACCESSORY",
+        _cap: "1",
+      },
+    ],
+    _avail: "2",
+    _cost: "2000",
+    _id: "smartlink",
+  },
+  {
+    useas: [
+      {
+        _type: "ACCESSORY",
+        _subtype: "OPTICAL",
+        _cap: "1",
+        _slot: "OPTICAL",
+      },
+      {
+        _type: "ACCESSORY",
+        _subtype: "CYBER_EYEWARE",
+        _cap: "2",
+        _slot: "CYBEREYE_IMPLANT",
+        _cost: "1500",
+      },
+      {
+        _type: "CYBERWARE",
+        _subtype: "CYBER_EYEWARE",
+        _ess: "0.1",
+        _cost: "1500",
+      },
+      {
+        _type: "ACCESSORY",
+        _subtype: "OPTICAL",
+        _slot: "HELMET_ACCESSORY",
+        _cap: "1",
+      },
+    ],
+    _avail: "2",
+    _cost: "500",
+    _id: "thermographic_vision",
+  },
+  {
+    useas: [
+      {
+        _type: "ACCESSORY",
+        _subtype: "OPTICAL",
+        _cap: "1",
+        _slot: "OPTICAL",
+      },
+      {
+        _type: "ACCESSORY",
+        _subtype: "OPTICAL",
+        _slot: "HELMET_ACCESSORY",
+        _cap: "1",
+      },
+    ],
+    _avail: "1",
+    _cost: "300",
+    _id: "ultrasound_link",
+  },
+  {
+    useas: [
+      {
+        _type: "ACCESSORY",
+        _subtype: "OPTICAL",
+        _cap: "2",
+        _slot: "OPTICAL",
+      },
+      {
+        _type: "ACCESSORY",
+        _subtype: "CYBER_EYEWARE",
+        _cap: "2",
+        _slot: "CYBEREYE_IMPLANT",
+        _cost: "3500",
+        _avail: "3",
+      },
+      {
+        _type: "CYBERWARE",
+        _subtype: "CYBER_EYEWARE",
+        _ess: "0.1",
+        _cost: "3500",
+        _avail: "3",
+      },
+      {
+        _type: "ACCESSORY",
+        _subtype: "OPTICAL",
+        _slot: "HELMET_ACCESSORY",
+        _cap: "2",
+      },
+    ],
+    _avail: "1",
+    _cost: "500",
+    _id: "vision_enhancement",
+  },
+  {
+    useas: [
+      {
+        _type: "ACCESSORY",
+        _subtype: "OPTICAL",
+        _cap: "1",
+        _slot: "OPTICAL",
+      },
+      {
+        _type: "ACCESSORY",
+        _subtype: "CYBER_EYEWARE",
+        _cap: "2",
+        _slot: "CYBEREYE_IMPLANT",
+        _cost: "1750",
+        _avail: "3",
+      },
+      {
+        _type: "CYBERWARE",
+        _subtype: "CYBER_EYEWARE",
+        _ess: "0.1",
+        _cost: "1750",
+        _avail: "3",
+      },
+      {
+        _type: "ACCESSORY",
+        _subtype: "OPTICAL",
+        _slot: "HELMET_ACCESSORY",
+        _cap: "1",
+      },
+    ],
+    modifications: {
+      attrmod: {
+        _attr: "ATTACK_RATING",
+        _ar: "0,0,2,2,2",
+      },
+    },
+    _avail: "1",
+    _cost: "250",
+    _id: "vision_magnification",
+  },
+  {
+    useas: [
+      {
+        _type: "ELECTRONICS",
+        _subtype: "AUDIO",
+      },
+    ],
+    modifications: {
+      itemhookmod: {
+        _capacity: "1",
+        _hook: "AUDIO",
+      },
+    },
+    _avail: "2",
+    _cost: "50",
+    _id: "directional_microphone",
+    _maxrat: "6",
+    _rate: "true",
+    _ratemul: "CAPACITY PRICE",
+  },
+  {
+    useas: [
+      {
+        _type: "ELECTRONICS",
+        _subtype: "AUDIO",
+      },
+    ],
+    modifications: {
+      itemhookmod: {
+        _capacity: "1",
+        _hook: "AUDIO",
+      },
+    },
+    _avail: "1",
+    _cost: "50",
+    _id: "earbuds",
+    _maxrat: "3",
+    _rate: "true",
+    _ratemul: "CAPACITY PRICE",
+  },
+  {
+    useas: [
+      {
+        _type: "ELECTRONICS",
+        _subtype: "AUDIO",
+      },
+    ],
+    modifications: {
+      itemhookmod: {
+        _capacity: "1",
+        _hook: "AUDIO",
+      },
+    },
+    _avail: "1",
+    _cost: "50",
+    _id: "headphones",
+    _maxrat: "6",
+    _rate: "true",
+    _ratemul: "CAPACITY PRICE",
+  },
+  {
+    useas: [
+      {
+        _type: "ELECTRONICS",
+        _subtype: "AUDIO",
+      },
+    ],
+    modifications: {
+      itemhookmod: {
+        _capacity: "1",
+        _hook: "AUDIO",
+      },
+    },
+    _avail: "2",
+    _cost: "100",
+    _id: "laser_microphone",
+    _maxrat: "6",
+    _rate: "true",
+    _ratemul: "CAPACITY PRICE",
+  },
+  {
+    useas: [
+      {
+        _type: "ELECTRONICS",
+        _subtype: "AUDIO",
+      },
+    ],
+    modifications: {
+      itemhookmod: {
+        _capacity: "1",
+        _hook: "AUDIO",
+      },
+    },
+    _avail: "1",
+    _cost: "50",
+    _id: "omnidirectional_microphone",
+    _maxrat: "6",
+    _rate: "true",
+    _ratemul: "CAPACITY PRICE",
+  },
+  {
+    useas: [
+      {
+        _type: "ACCESSORY",
+        _subtype: "AUDIO_ENHANCEMENT",
+        _cap: "1",
+        _slot: "AUDIO",
+      },
+      {
+        _type: "ACCESSORY",
+        _subtype: "CYBER_EARWARE",
+        _cap: "1",
+        _slot: "CYBEREAR_IMPLANT",
+        _cost: "3500",
+        _avail: "2",
+      },
+      {
+        _type: "CYBERWARE",
+        _subtype: "CYBER_EARWARE",
+        _ess: "0.1",
+        _cost: "3500",
+        _avail: "2",
+      },
+    ],
+    _avail: "1",
+    _cost: "500",
+    _id: "audio_enhancement",
+  },
+  {
+    useas: [
+      {
+        _type: "ACCESSORY",
+        _subtype: "AUDIO_ENHANCEMENT",
+        _cap: "1",
+        _slot: "AUDIO",
+      },
+      {
+        _type: "ACCESSORY",
+        _subtype: "CYBER_EARWARE",
+        _cap: "1",
+        _slot: "CYBEREAR_IMPLANT",
+        _cost: "3250",
+        _avail: "4",
+        _maxrat: "6",
+      },
+      {
+        _type: "CYBERWARE",
+        _subtype: "CYBER_EARWARE",
+        _ess: "0.1",
+        _cost: "3250",
+        _avail: "4",
+      },
+    ],
+    _avail: "3",
+    _cost: "250",
+    _id: "select_sound_filter",
+    _maxrat: "3",
+    _rate: "true",
+    _ratemul: "CAPACITY PRICE",
+  },
+  {
+    useas: [
+      {
+        _type: "ACCESSORY",
+        _subtype: "AUDIO_ENHANCEMENT",
+        _cap: "2",
+        _slot: "AUDIO",
+      },
+      {
+        _type: "ACCESSORY",
+        _subtype: "CYBER_EARWARE",
+        _cap: "2",
+        _slot: "CYBEREAR_IMPLANT",
+        _cost: "3000",
+        _avail: "2",
+      },
+      {
+        _type: "CYBERWARE",
+        _subtype: "CYBER_EARWARE",
+        _ess: "0.1",
+        _cost: "3000",
+        _avail: "2",
+      },
+    ],
+    _avail: "2",
+    _cost: "1000",
+    _id: "spatial_recognizer",
+    _subtype: "AUDIO_ENHANCEMENT",
+    _type: "ACCESSORY",
+  },
+  {
+    useas: [
+      {
+        _type: "ELECTRONICS",
+        _subtype: "SENSOR_HOUSING",
+      },
+    ],
+    modifications: {
+      itemhookmod: {
+        _capacity: "1",
+        _hook: "SENSOR_HOUSING",
+      },
+    },
+    _avail: "1",
+    _cost: "100",
+    _id: "handheld_housing",
+    _maxrat: "3",
+    _rate: "true",
+    _ratemul: "CAPACITY PRICE",
+    _sensrat: "3",
+  },
+  {
+    useas: [
+      {
+        _type: "ELECTRONICS",
+        _subtype: "SENSOR_HOUSING",
+      },
+    ],
+    modifications: {
+      itemhookmod: {
+        _capacity: "1",
+        _hook: "SENSOR_HOUSING",
+      },
+    },
+    _avail: "1",
+    _cost: "250",
+    _id: "wall-mounted_housing",
+    _maxrat: "6",
+    _rate: "true",
+    _ratemul: "CAPACITY PRICE",
+    _sensrat: "4",
+  },
+  {
+    useas: [
+      {
+        _type: "ACCESSORY",
+        _subtype: "SENSOR",
+        _slot: "SENSOR_HOUSING",
+        _cap: "1",
+      },
+    ],
+    modifications: {
+      itemhookmod: {
+        _capacity: "1",
+        _hook: "SENSOR_FUNCTION",
+      },
+    },
+    _avail: "3",
+    _cost: "1000",
+    _id: "sensor_array",
+    _minrat: "2",
+    _maxrat: "8",
+    _rate: "true",
+    _ratemul: "CAPACITY PRICE",
+  },
+  {
+    useas: [
+      {
+        _type: "ACCESSORY",
+        _subtype: "SENSOR",
+        _slot: "SENSOR_HOUSING",
+        _cap: "1",
+      },
+    ],
+    modifications: {
+      itemhookmod: {
+        _capacity: "1",
+        _hook: "SENSOR_FUNCTION",
+      },
+    },
+    _avail: "2",
+    _cost: "100",
+    _id: "single_sensor",
+    _maxrat: "8",
+    _rate: "true",
+    _ratemul: "PRICE",
+    _multi: "true",
+  },
+  {
+    useas: [
+      {
+        _type: "ACCESSORY",
+        _subtype: "SENSOR",
+        _slot: "SENSOR_FUNCTION",
+        _cap: "1",
+      },
+    ],
+    _id: "atmosphere_sensor",
+    _cost: "0",
+  },
+  {
+    useas: [
+      {
+        _type: "ACCESSORY",
+        _subtype: "SENSOR",
+        _slot: "SENSOR_FUNCTION",
+        _cap: "1",
+      },
+    ],
+    modifications: {
+      itemhookmod: {
+        _capacity: "1",
+        _hook: "OPTICAL",
+      },
+    },
+    _id: "camera_function",
+    _cost: "0",
+  },
+  {
+    useas: [
+      {
+        _type: "ACCESSORY",
+        _subtype: "SENSOR",
+        _slot: "SENSOR_FUNCTION",
+        _cap: "1",
+      },
+    ],
+    _id: "cyberware_scanner",
+    _cost: "0",
+  },
+  {
+    useas: [
+      {
+        _type: "ACCESSORY",
+        _subtype: "SENSOR",
+        _slot: "SENSOR_FUNCTION",
+        _cap: "1",
+      },
+    ],
+    modifications: {
+      itemhookmod: {
+        _capacity: "1",
+        _hook: "AUDIO",
+      },
+    },
+    _id: "directional_microphone_function",
+    _cost: "0",
+  },
+  {
+    useas: [
+      {
+        _type: "ACCESSORY",
+        _subtype: "SENSOR",
+        _slot: "SENSOR_FUNCTION",
+        _cap: "1",
+      },
+    ],
+    _id: "geiger_counter",
+    _cost: "0",
+  },
+  {
+    useas: [
+      {
+        _type: "ACCESSORY",
+        _subtype: "SENSOR",
+        _slot: "SENSOR_FUNCTION",
+        _cap: "1",
+      },
+    ],
+    modifications: {
+      itemhookmod: {
+        _capacity: "1",
+        _hook: "AUDIO",
+      },
+    },
+    _id: "laser_microphone_function",
+    _cost: "0",
+  },
+  {
+    useas: [
+      {
+        _type: "ACCESSORY",
+        _subtype: "SENSOR",
+        _slot: "SENSOR_FUNCTION",
+        _cap: "1",
+      },
+    ],
+    _id: "laser_range_finder",
+    _cost: "0",
+  },
+  {
+    useas: [
+      {
+        _type: "ACCESSORY",
+        _subtype: "SENSOR",
+        _slot: "SENSOR_FUNCTION",
+        _cap: "1",
+      },
+    ],
+    _id: "mad_scanner",
+    _cost: "0",
+  },
+  {
+    useas: [
+      {
+        _type: "ACCESSORY",
+        _subtype: "SENSOR",
+        _slot: "SENSOR_FUNCTION",
+        _cap: "1",
+      },
+    ],
+    _id: "motion_sensor",
+    _cost: "0",
+  },
+  {
+    useas: [
+      {
+        _type: "ACCESSORY",
+        _subtype: "SENSOR",
+        _slot: "SENSOR_FUNCTION",
+        _cap: "1",
+      },
+    ],
+    _id: "olfactory_sensor",
+    _cost: "0",
+  },
+  {
+    useas: [
+      {
+        _type: "ACCESSORY",
+        _subtype: "SENSOR",
+        _slot: "SENSOR_FUNCTION",
+        _cap: "1",
+      },
+    ],
+    modifications: {
+      itemhookmod: {
+        _capacity: "1",
+        _hook: "AUDIO",
+      },
+    },
+    _id: "omnidirectional_microphone_function",
+    _cost: "0",
+  },
+  {
+    useas: [
+      {
+        _type: "ACCESSORY",
+        _subtype: "SENSOR",
+        _slot: "SENSOR_FUNCTION",
+        _cap: "1",
+      },
+    ],
+    _id: "ultrasound_sensor_function",
+    _cost: "0",
+  },
+]
