b3601c686baf6f4b5abba40cb2d180dab31e4d70
dethstrobe <dethstrobe@gmail.com>
2021-04-15 00:16:12 -0400

    added ability to buy and sell mods on gear

--- components/runner/resources/electronics/Imaging/ImagingMods/ImagingMods.test.tsx
+++ components/runner/resources/electronics/Imaging/ImagingMods/ImagingMods.test.tsx
@@ -10,14 +10,44 @@ import ImagingMods from "."
 describe("ImagingMods", () => {
   beforeAll(setupIndexedDB)
   const setup = () => {
-    return render(withTestRouter(<ImagingMods />, { query: { id: "10" } }))
+    return render(
+      withTestRouter(<ImagingMods />, { query: { id: "10", gearIndex: "0" } }),
+    )
   }
   it("should display a list of imaging mods", async () => {
-    const { getByText } = setup()
+    const { getAllByText } = setup()
 
-    await waitFor(() => expect(getByText("Name")).toBeInTheDocument())
+    await waitFor(() => expect(getAllByText("Buy").length).toBeGreaterThan(0))
     imagingMods.forEach(({ name }) => {
-      expect(getByText(name)).toBeInTheDocument()
+      expect(getAllByText(name).length).toBeGreaterThan(0)
     })
+  })
+
+  it("should add a mod to a piece of gear", async () => {
+    const { getByText, getByLabelText } = setup()
+
+    await waitFor(() => expect(getByText("Buy")).toBeInTheDocument())
+    expect(getByText("1/3")).toBeInTheDocument()
+
+    getByLabelText("Add Smartlink").click()
+
+    await waitFor(() =>
+      expect(getByLabelText("Remove Smartlink")).toBeInTheDocument(),
+    )
+    expect(getByText("3/3")).toBeInTheDocument()
+  })
+
+  it("should remove a mod from a piece of gear", async () => {
+    const { getByText, getByLabelText, queryByLabelText } = setup()
+
+    await waitFor(() => expect(getByText("Sell")).toBeInTheDocument())
+
+    expect(getByText("3/3")).toBeInTheDocument()
+
+    getByLabelText("Remove Image Link").click()
+
+    expect(queryByLabelText("Remove Image Link")).not.toBeInTheDocument()
+
+    expect(getByText("2/3")).toBeInTheDocument()
   })
 })
