5a9ce96ed41fc35715f761116c0dfc4e1954325b
dethstrobe <dethstrobe@gmail.com>
2021-04-17 12:48:01 -0400

    add audio device data

---
+++ data/audio.ts
@@ -0,0 +1,89 @@
+import { GearModdableRated } from "@/types/Resources"
+
+export const audio: GearModdableRated[] = [
+  {
+    type: "ELECTRONICS",
+    subtype: "AUDIO",
+
+    modifications: {
+      itemhookmod: {
+        capacity: 1,
+        hook: "AUDIO",
+      },
+    },
+    availability: "2",
+    cost: 50,
+    name: "Directional Microphone",
+    maxRating: 6,
+    rating: true,
+    rateMultiplier: "capacity cost",
+  },
+  {
+    type: "ELECTRONICS",
+    subtype: "AUDIO",
+
+    modifications: {
+      itemhookmod: {
+        capacity: 1,
+        hook: "AUDIO",
+      },
+    },
+    availability: "1",
+    cost: 50,
+    name: "Earbuds",
+    maxRating: 3,
+    rating: true,
+    rateMultiplier: "capacity cost",
+  },
+  {
+    type: "ELECTRONICS",
+    subtype: "AUDIO",
+
+    modifications: {
+      itemhookmod: {
+        capacity: 1,
+        hook: "AUDIO",
+      },
+    },
+    availability: "1",
+    cost: 50,
+    name: "Headphones",
+    maxRating: 6,
+    rating: true,
+    rateMultiplier: "capacity cost",
+  },
+  {
+    type: "ELECTRONICS",
+    subtype: "AUDIO",
+
+    modifications: {
+      itemhookmod: {
+        capacity: 1,
+        hook: "AUDIO",
+      },
+    },
+    availability: "2",
+    cost: 100,
+    name: "Laser Microphone",
+    maxRating: 6,
+    rating: true,
+    rateMultiplier: "capacity cost",
+  },
+  {
+    type: "ELECTRONICS",
+    subtype: "AUDIO",
+
+    modifications: {
+      itemhookmod: {
+        capacity: 1,
+        hook: "AUDIO",
+      },
+    },
+    availability: "1",
+    cost: 50,
+    name: "Omnidirectional Microphone",
+    maxRating: 6,
+    rating: true,
+    rateMultiplier: "capacity cost",
+  },
+]
