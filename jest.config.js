24e8df2bb18320fb70a3cc768c29d875bc0d9640
dethstrobe <dethstrobe@gmail.com>
2021-05-29 22:09:48 -0400

    bump version of jest to 27.0.3

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   roots: ["<rootDir>"],
   moduleFileExtensions: ["js", "ts", "tsx", "json"],
+  testEnvironment: "jsdom",
   testPathIgnorePatterns: ["<rootDir>[/\\\\](node_modules|.next)[/\\\\]"],
   transformIgnorePatterns: ["[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$"],
   transform: {
