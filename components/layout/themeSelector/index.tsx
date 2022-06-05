3745242e0110385fdc58f8de1d6c43a9752eacca
dethstrobe <dethstrobe@gmail.com>
2022-06-05 15:24:56 -0400

    refactor themeing context provider to pass in a state hook

--- components/layout/themeSelector/index.tsx
+++ components/layout/themeSelector/index.tsx
@@ -1,15 +1,17 @@
 import { MenuItem, Select } from "@mui/material"
 import { createContext, FC, useContext } from "react"
 
-export const ThemeingContext = createContext<(themeName: string) => void>(null)
+type ThemeState = [
+  currentSelectedTheme: string,
+  setTheme: (themeName: string) => void,
+]
+
+export const ThemeingContext = createContext<ThemeState>(null)
 
 export const ThemeSelector: FC = () => {
-  const setTheme = useContext(ThemeingContext)
+  const [theme = "cyberterminal3", setTheme] = useContext(ThemeingContext)
   return (
-    <Select
-      value="cyberterminal3"
-      onChange={({ target: { value } }) => setTheme(value)}
-    >
+    <Select value={theme} onChange={({ target: { value } }) => setTheme(value)}>
       <MenuItem value="cyberterminal3">CyberTerminal 3.0</MenuItem>
       <MenuItem value="mundane">Mundane</MenuItem>
     </Select>
--- components/layout/themeSelector/theme.test.tsx
+++ components/layout/themeSelector/theme.test.tsx
@@ -2,19 +2,19 @@ import { render, userEvent } from "@/test/testUtils"
 import { ThemeSelector, ThemeingContext } from "."
 
 describe("<ThemeSelector />", () => {
-  const setup = () => {
+  const setup = (mockState?: string) => {
     const mockUpdater = jest.fn()
     return {
       mockUpdater,
       ...render(
-        <ThemeingContext.Provider value={mockUpdater}>
+        <ThemeingContext.Provider value={[mockState, mockUpdater]}>
           <ThemeSelector />
         </ThemeingContext.Provider>,
       ),
     }
   }
   it("should display a select with different theme names", async () => {
-    const { getByText, mockUpdater } = setup()
+    const { getByText, mockUpdater } = setup("cyberterminal3")
     const selectButton = getByText("CyberTerminal 3.0")
     expect(selectButton).toBeInTheDocument()
     await userEvent.click(selectButton)
@@ -23,5 +23,10 @@ describe("<ThemeSelector />", () => {
     await userEvent.click(selection)
 
     expect(mockUpdater).toHaveBeenCalledWith("mundane")
+  })
+
+  it("should default to cyberterminal3 if no theme is provided", () => {
+    const { getByText } = setup()
+    expect(getByText("CyberTerminal 3.0")).toBeInTheDocument()
   })
 })
