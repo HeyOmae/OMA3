ad5c88f30f3cf731bd578f886461c1c2d614ba5f
dethstrobe <dethstrobe@gmail.com>
2023-07-02 15:59:02 -0400

    implement Sensitive System quality

--- components/runner/resources/GearPageTemplate/RemainingEssence/index.tsx
+++ components/runner/resources/GearPageTemplate/RemainingEssence/index.tsx
@@ -9,19 +9,29 @@ const startingEss = 6
 
 export const RemainingEssence: FC<Props> = ({ runner }) => {
   const essence = useMemo(() => {
+    const senstiveSystemModifier = runner.qualities?.negative?.some(
+      ({ name }) => name === "Sensitive System",
+    )
+      ? 2
+      : 1
     const usedEssFromCyber =
       runner.resources?.cyberware?.reduce(
         (ess, { useAs, currentRating = 1 }) => {
           const useage = useAs.find((mod) => "essence" in mod)
           const modifier = currentRating
-          return ess - +(useage as GearAugmentationUseAs).essence * modifier
+          return (
+            ess -
+            +(useage as GearAugmentationUseAs).essence *
+              modifier *
+              senstiveSystemModifier
+          )
         },
         startingEss,
       ) ?? startingEss
     const remainingEss =
       runner.resources?.bioware?.reduce(
         (ess, { useAs: { essence }, currentRating = 1 }) =>
-          ess - +essence * currentRating,
+          ess - +essence * currentRating * senstiveSystemModifier,
         usedEssFromCyber,
       ) ?? usedEssFromCyber
     // rounds the essence value to the nearist hundredth
