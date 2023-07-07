755d987f5bba393c56b3b956fc8c692590938050
dethstrobe <dethstrobe@gmail.com>
2023-07-06 22:12:07 -0400

    display number of qualities purchased

---
+++ components/runner/resources/GearPageTemplate/RamainingKarma/RemainingQualities.test.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from "@/test/testUtils"
+import { RemainingKarma } from "./index"
+import { Runner } from "@/types/runner"
+import { mockedRunners } from "@/test/mocks"
+
+const setup = (runner: Runner = mockedRunners[0]) => {
+  render(<RemainingKarma runner={runner} />)
+}
+
+test("display the number of qualitys on a runner", () => {
+  setup()
+
+  expect(
+    screen.getByRole("term", { name: /Qualities/i }).closest("dl"),
+  ).toHaveTextContent("6/6")
+})
+
+test("number of qualities should be reduced by qualities on the runner", () => {
+  setup(mockedRunners[3])
+
+  expect(
+    screen.getByRole("definition", { name: "Qualities Value" }),
+  ).toHaveTextContent("0/6")
+})
