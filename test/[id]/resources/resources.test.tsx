ceb0c28bebc37f20585a76ac9c566b5d3c163710
dethstrobe <dethstrobe@gmail.com>
2023-07-29 21:12:05 -0400

    implement karma to nuyen in to resource page

--- test/[id]/resources/resources.test.tsx
+++ test/[id]/resources/resources.test.tsx
@@ -1,11 +1,17 @@
-import { render, setupIndexedDB, withTestRouter } from "../../testUtils"
+import {
+  render,
+  setupIndexedDB,
+  withTestRouter,
+  screen,
+  userEvent,
+} from "../../testUtils"
 import ResourcePage from "@/pages/[id]/resources"
-import { waitFor } from "@testing-library/react"
 import { mockedRunners } from "@/test/mocks"
 
 describe("Resource Page", () => {
   beforeAll(setupIndexedDB)
-  const setup = () =>
+  const setup = () => {
+    const user = userEvent.setup()
     render(
       withTestRouter(<ResourcePage />, {
         query: { id: "10" },
@@ -13,70 +19,71 @@ describe("Resource Page", () => {
         asPath: "/10/resources",
       }),
     )
+    return user
+  }
 
   const gear = Object.entries(mockedRunners[9].resources)
 
   it("should exist", async () => {
-    const { getByText, getAllByText } = setup()
+    setup()
 
     // wait for the runner to load from indexeddb
-    await waitFor(() => getByText(gear[0][0]))
+    expect(await screen.findByText(gear[0][0])).toBeInTheDocument()
 
-    expect(getByText("Weapons")).toBeInTheDocument()
+    expect(screen.getByText("Weapons")).toBeInTheDocument()
 
-    expect(getByText("Melee Weapons").closest("a")).toHaveAttribute(
+    expect(screen.getByText("Melee Weapons").closest("a")).toHaveAttribute(
       "href",
       "/10/resources/melee",
     )
-    expect(getByText("Projectile Weapons").closest("a")).toHaveAttribute(
+    expect(screen.getByText("Projectile Weapons").closest("a")).toHaveAttribute(
       "href",
       "/10/resources/projectile",
     )
-    expect(getByText("Firearms").closest("a")).toHaveAttribute(
+    expect(screen.getByText("Firearms").closest("a")).toHaveAttribute(
       "href",
       "/10/resources/firearms",
     )
-    expect(getByText("Armors").closest("a")).toHaveAttribute(
+    expect(screen.getByText("Armors").closest("a")).toHaveAttribute(
       "href",
       "/10/resources/armor",
     )
-    expect(getByText("Commlinks").closest("a")).toHaveAttribute(
+    expect(screen.getByText("Commlinks").closest("a")).toHaveAttribute(
       "href",
       "/10/resources/commlinks",
     )
-    expect(getByText("Cyberdecks").closest("a")).toHaveAttribute(
+    expect(screen.getByText("Cyberdecks").closest("a")).toHaveAttribute(
       "href",
       "/10/resources/cyberdecks",
     )
-    expect(getByText("Rigger Consoles").closest("a")).toHaveAttribute(
+    expect(screen.getByText("Rigger Consoles").closest("a")).toHaveAttribute(
       "href",
       "/10/resources/consoles",
     )
-    expect(getByText("Electronic Accessories").closest("a")).toHaveAttribute(
-      "href",
-      "/10/resources/eaccessories",
-    )
-    expect(getByText("RFID Tags").closest("a")).toHaveAttribute(
+    expect(
+      screen.getByText("Electronic Accessories").closest("a"),
+    ).toHaveAttribute("href", "/10/resources/eaccessories")
+    expect(screen.getByText("RFID Tags").closest("a")).toHaveAttribute(
       "href",
       "/10/resources/tags",
     )
-    expect(getByText("Other Electronics").closest("a")).toHaveAttribute(
+    expect(screen.getByText("Other Electronics").closest("a")).toHaveAttribute(
       "href",
       "/10/resources/other-electronics",
     )
-    expect(getByText("Tools").closest("a")).toHaveAttribute(
+    expect(screen.getByText("Tools").closest("a")).toHaveAttribute(
       "href",
       "/10/resources/tools",
     )
-    expect(getByText("Imaging Devices").closest("a")).toHaveAttribute(
+    expect(screen.getByText("Imaging Devices").closest("a")).toHaveAttribute(
       "href",
       "/10/resources/imaging",
     )
-    expect(getByText("Audio Devices").closest("a")).toHaveAttribute(
+    expect(screen.getByText("Audio Devices").closest("a")).toHaveAttribute(
       "href",
       "/10/resources/audio",
     )
-    expect(getByText("Sensor Devices").closest("a")).toHaveAttribute(
+    expect(screen.getByText("Sensor Devices").closest("a")).toHaveAttribute(
       "href",
       "/10/resources/sensor",
     )
@@ -84,120 +91,136 @@ describe("Resource Page", () => {
     // security stuff in other test section
 
     // I should come up with a smarter way to do this, but I can't right now
-    expect(getAllByText("Biotech")[1].closest("a")).toHaveAttribute(
+    expect(screen.getAllByText("Biotech")[1].closest("a")).toHaveAttribute(
       "href",
       "/10/resources/biotech",
     )
 
     // Rigger stuff
-    expect(getByText("Drones").closest("a")).toHaveAttribute(
+    expect(screen.getByText("Drones").closest("a")).toHaveAttribute(
       "href",
       "/10/resources/drones",
     )
-    expect(getByText("Vehicles").closest("a")).toHaveAttribute(
+    expect(screen.getByText("Vehicles").closest("a")).toHaveAttribute(
       "href",
       "/10/resources/vehicles",
     )
 
     // Augmentations
-    expect(getByText("Cyberware").closest("a")).toHaveAttribute(
+    expect(screen.getByText("Cyberware").closest("a")).toHaveAttribute(
       "href",
       "/10/resources/cyberware",
     )
-    expect(getByText("Bioware").closest("a")).toHaveAttribute(
+    expect(screen.getByText("Bioware").closest("a")).toHaveAttribute(
       "href",
       "/10/resources/bioware",
     )
 
-    expect(getByText("Lifestyles").closest("a")).toHaveAttribute(
+    expect(screen.getByText("Lifestyles").closest("a")).toHaveAttribute(
       "href",
       "/10/resources/lifestyle",
     )
   })
 
   it("should render links to security devices", async () => {
-    const { getByText } = setup()
+    setup()
 
     // wait for the runner to load from indexeddb
-    await waitFor(() => getByText(gear[0][0]))
+    expect(await screen.findByText(gear[0][0])).toBeInTheDocument()
 
-    expect(getByText("locks").closest("a")).toHaveAttribute(
+    expect(screen.getByText("locks").closest("a")).toHaveAttribute(
       "href",
       "/10/resources/security/locks",
     )
-    expect(getByText("restraints").closest("a")).toHaveAttribute(
+    expect(screen.getByText("restraints").closest("a")).toHaveAttribute(
       "href",
       "/10/resources/security/restraints",
     )
-    expect(getByText("bne").closest("a")).toHaveAttribute(
+    expect(screen.getByText("bne").closest("a")).toHaveAttribute(
       "href",
       "/10/resources/security/bne",
     )
-    expect(getByText("chemicals").closest("a")).toHaveAttribute(
+    expect(screen.getByText("chemicals").closest("a")).toHaveAttribute(
       "href",
       "/10/resources/security/chemicals",
     )
-    expect(getByText("survival").closest("a")).toHaveAttribute(
+    expect(screen.getByText("survival").closest("a")).toHaveAttribute(
       "href",
       "/10/resources/security/survival",
     )
-    expect(getByText("grappleGun").closest("a")).toHaveAttribute(
+    expect(screen.getByText("grappleGun").closest("a")).toHaveAttribute(
       "href",
       "/10/resources/security/grappleGun",
     )
-    expect(getByText("toxins").closest("a")).toHaveAttribute(
+    expect(screen.getByText("toxins").closest("a")).toHaveAttribute(
       "href",
       "/10/resources/security/toxins",
     )
-    expect(getByText("drugs").closest("a")).toHaveAttribute(
+    expect(screen.getByText("drugs").closest("a")).toHaveAttribute(
       "href",
       "/10/resources/security/drugs",
     )
   })
 
   it("should render magical gear", async () => {
-    const { getByText } = setup()
+    setup()
 
     // wait for the runner to load from indexeddb
-    await waitFor(() => getByText(gear[0][0]))
+    expect(await screen.findByText(gear[0][0])).toBeInTheDocument()
 
     // This one is hard coded
-    expect(getByText("Foci").closest("a")).toHaveAttribute(
+    expect(screen.getByText("Foci").closest("a")).toHaveAttribute(
       "href",
       "/10/resources/foci",
     )
 
     // These ones are read from the MagicGear data file
-    expect(getByText("magicSupplies").closest("a")).toHaveAttribute(
+    expect(screen.getByText("magicSupplies").closest("a")).toHaveAttribute(
       "href",
       "/10/resources/magic/magicSupplies",
     )
-    expect(getByText("spellFormula").closest("a")).toHaveAttribute(
+    expect(screen.getByText("spellFormula").closest("a")).toHaveAttribute(
       "href",
       "/10/resources/magic/spellFormula",
     )
-    expect(getByText("focusFormula").closest("a")).toHaveAttribute(
+    expect(screen.getByText("focusFormula").closest("a")).toHaveAttribute(
       "href",
       "/10/resources/magic/focusFormula",
     )
   })
 
   it("should display all the gear a runner has purchased", async () => {
-    const { getByText } = setup()
+    setup()
 
     // Grab the first category and make sure it appears
-    await waitFor(() => getByText(gear[0][0]))
+    expect(await screen.findByText(gear[0][0])).toBeInTheDocument()
 
     gear.forEach(([resourceKey, gearArray]) => {
-      const resourceType = getByText(resourceKey)
+      const resourceType = screen.getByText(resourceKey)
       expect(resourceType.closest("a")).toHaveAttribute(
         "href",
         `/10/resources/${resourceKey}`,
       )
 
       gearArray.forEach(({ name }) =>
-        expect(getByText(name)).toBeInTheDocument(),
+        expect(screen.getByText(name)).toBeInTheDocument(),
       )
     })
+  })
+
+  it("should allow a runner to raise nuyen by spending karma", async () => {
+    const user = setup()
+
+    expect(
+      await screen.findByRole("definition", { name: "Nuyen Value" }),
+    ).toHaveTextContent("275000¥")
+
+    await user.click(
+      screen.getByRole("button", { name: "Increment Karma to Nuyen" }),
+    )
+
+    expect(
+      screen.getByRole("definition", { name: "Nuyen Value" }),
+    ).toHaveTextContent("277000¥")
   })
 })
