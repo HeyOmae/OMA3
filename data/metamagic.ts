1ef5403fcc440509d7518ae399bd4b91064cd296
dethstrobe <dethstrobe@gmail.com>
2023-09-02 11:36:13 -0400

    add metamagic data

---
+++ data/metamagic.ts
@@ -0,0 +1,40 @@
+import { MetaMagic } from "@/types/MagRes"
+
+export const metamagic: MetaMagic[] = [
+  {
+    name: "Adept Centering",
+    type: "METAMAGIC_ADEPT",
+  },
+  {
+    name: "Centering",
+    type: "METAMAGIC",
+  },
+  {
+    name: "Fixation",
+    type: "METAMAGIC",
+  },
+  {
+    name: "Flexible Signature",
+    type: "METAMAGIC",
+  },
+  {
+    name: "Masking",
+    type: "METAMAGIC",
+  },
+  {
+    name: "Power Points",
+    type: "METAMAGIC_ADEPT",
+  },
+  {
+    name: "Quickening",
+    type: "METAMAGIC",
+  },
+  {
+    name: "Spell Shapening",
+    type: "METAMAGIC",
+  },
+  {
+    name: "Shielding",
+    type: "METAMAGIC",
+  },
+]
