ab4a6aea07706b94a737d143b05c6763363d5c28
dethstrobe <dethstrobe@gmail.com>
2025-02-02 11:39:35 -0500

    removed web XR experiment

--- pages/api/xr.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next"
-import { html } from "@/xr/index"
-
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<string>,
-) {
-  res.status(200).send(html)
-}
